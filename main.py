c0137c2a5fb27310b0bbac36442eca8c56086397
Soham Sangole <sohamajaysangole@gmail.com>
2024-05-02 05:48:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=2,detectionCon=0.8)
+
+while True:
+    success, img = cap.read()
+    hands,img = detector.findHands(img)
+
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
